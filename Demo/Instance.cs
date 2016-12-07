e7a9e3876d10ca98bdd8fc0d4faa15264297d624
jbo2014 <jbo2014haha@outlook.com>
2016-12-07 17:28:09 +0800

    WO
    20161207

---
+++ Demo/Instance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Demo
+{
+    class Instance
+    {
+        public string LoadStrXml()
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load(@"G:\GitHubs\WeSys\Demo\diagram.bpmn20.xml");
+            string strXml = xml.InnerXml;
+            return strXml;
+        }
+
+        public void run()
+        {
+
+            Console.Read();
+        }
+    }
+}
